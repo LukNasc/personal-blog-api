5ef5ce89ae8ef65f4ec3f6a87b4b66920c9faf47
Lucas Nascimento <lucasjoao85@gmail.com>
2020-09-16 21:33:14 -0300

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-import {Response, Request, NextFunction} from 'express'
-export default module.exports = (req: Request, res:Response, next:NextFunction) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  })
-}
