8fcae4ccaffb48b5a4fc43c0c48aa6f67d3bc951
Lucas Nascimento <joao.nascimento@gfxconsultoria.com>
2020-10-01 15:24:30 -0300

    Ajustando rota de getArticleById

--- src/routes.ts
+++ src/routes.ts
@@ -17,6 +17,6 @@ router.post("/users/login", UsersController.login);
 // Articles
 router.post("/articles/store", [verifyJWT], ArticlesController.store);
 router.get("/articles/list", ArticlesController.list);
-router.get("/articles/getArticleById:ID", ArticlesController.getArticleById);
+router.get("/articles/getArticleById", ArticlesController.getArticleById);
 
 export default module.exports = router;
